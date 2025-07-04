834213296cb859fa4b1a940f6a0c387017213cd0
David Tran <tranhuyducseven@gmail.com>
2025-07-04 11:46:22 +0700

    feat: prepare script

--- script/PrepareLiquidity.s.sol
+++ script/PrepareLiquidity.s.sol
@@ -6,6 +6,7 @@ import { FixedPriceOracle } from "../src/oracles/FixedPriceOracle.sol";
 import { DegenBox } from "../src/DegenBox.sol";
 import { BaseScript } from "./Base.s.sol";
 import { IMintableBurnableERC20 } from "../src/interfaces/IMintableBurnableERC20.sol";
+import { ICauldronV2 } from "../src/interfaces/ICauldronV2.sol";
 import { console2 } from "forge-std/console2.sol";
 
 interface IDegenBox {
@@ -15,19 +16,31 @@ interface IDegenBox {
         address to,
         uint256 amount,
         uint256 share
-    ) external payable returns (uint256 amountOut, uint256 shareOut);
+    )
+        external
+        payable
+        returns (uint256 amountOut, uint256 shareOut);
+}
+
+interface IMarketLens {
+    function getCollateralPrice(ICauldronV2 cauldron) external view returns (uint256);
 }
 
 contract PrepareLiquidityScript is BaseScript {
-    address public constant SBTC = 0x3cBb62d23120918019037b51a1e513FdAAddDE3f;
-    address public constant SUSD = 0x124C4a03C08601a0625bb5b543E58b2a61fCE770;
-    address public constant DEGEN_BOX = 0xB97DfDF8b0692a2A9aff2fc089E120b05410C7BD;
-    address public constant SBTC_MARKET = 0x962dbc8209Eb083Ed71C38cf4d7482c7C947cF14;
+    address public constant SUSD = 0x68C8f8Cf7983b7caC2BE9Ba25817e211B3Da7377;
+    address public constant SBTC = 0x55df835d1e881dA3a75ad7C676c4233234133815;
+    address public constant DEGEN_BOX = 0x388873725F97BBEAa02ee082029ebdE72C654430;
+    address public constant SBTC_MARKET = 0x9D83C9F703dce6ea9efcE8473587233bfcd8ef51;
+    address public constant MARKET_LENS = 0x350f611839C26FA96DFb4C893a60cEeC7F2A483E;
 
-    function run() external broadcast(){
+    function run() external broadcast {
         console2.log("Broadcaster:", msg.sender);
-        // IMintableBurnableERC20(SBTC).mint(msg.sender, 1000 ether);
+        IMintableBurnableERC20(SBTC).mint(msg.sender, 1000 ether);
         IMintableBurnableERC20(SUSD).mint(msg.sender, 1e9 ether);
         IDegenBox(DEGEN_BOX).deposit(IMintableBurnableERC20(SUSD), msg.sender, SBTC_MARKET, 1e9 ether, 0);
+
+        uint256 collateralPrice = IMarketLens(MARKET_LENS).getCollateralPrice(ICauldronV2(SBTC_MARKET));
+
+        console2.log(collateralPrice);
     }
 }
